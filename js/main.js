55bf667f3a0ce18d8ef44e6ef00463e3d966a21b
Dzoter <Andry94@list.ru>
2022-10-09 14:43:31 +0400

    Создал функцию, которая создает необходимое кол-во объектов и возвращает их в виде массива

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+/**
+ * получение случайного числа в диапазоне
+ * @param numberOne первое число
+ * @param numberTwo второе число
+ * @returns {*|number} случайное число
+ */
 const getRandomInteger = (numberOne, numberTwo) => {
 
-
+  /**
+   * корректное вычисление большего из меньшего
+   * @returns {number} остаток
+   */
   const getRightCount = function () {
     if (numberOne > numberTwo) {
       return numberOne - numberTwo;
@@ -10,9 +19,12 @@ const getRandomInteger = (numberOne, numberTwo) => {
     return 0;
   };
 
+  /**
+   * определение меньшего числа из двух заданных
+   * @returns {*} меньшее число
+   */
   const getMinNumber = function () {
     return numberOne < numberTwo ? numberOne : numberTwo;
-
   };
 
 
@@ -22,10 +34,43 @@ const getRandomInteger = (numberOne, numberTwo) => {
 };
 getRandomInteger(1, 2);
 
-
+/**
+ * проверяет длину строки
+ * @param string строка
+ * @param maxLength максимальная длина
+ * @returns {boolean} значение, является ли длина строки меньше заданного значения
+ */
 const isRightLengthOfString = function (string, maxLength) {
 
   return string.length <= maxLength;
 };
 
 isRightLengthOfString('я тут был',10);
+
+
+/**
+ * Создание массива объектов.
+ * @param quantity кол-во созданных объектов
+ * @returns {*[]} возвращает массив объектов
+ */
+const createPhotoDescription = function (quantity )
+{
+  const photosList = [];
+
+  for (let i = 1; i < quantity; i++)
+  {
+    photosList[i - 1] = {
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: (Math.random() + 1).toString(36).substring(7),
+      likes: getRandomInteger(15, 200),
+      comments: getRandomInteger(0, 200)
+    };
+
+  }
+  return photosList;
+};
+
+const arrayOfPhotos = createPhotoDescription(25);
+
+console.table(arrayOfPhotos)
